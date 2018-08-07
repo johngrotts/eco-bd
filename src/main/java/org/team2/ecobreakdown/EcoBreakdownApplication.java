566e90a96bf8dacf66ad6ab2c3ed00809d7b44ac
johngrotts <johngrotts@users.noreply.github.com>
2018-08-07 16:35:26 -0500

    Initial Spring

---
+++ src/main/java/org/team2/ecobreakdown/EcoBreakdownApplication.java
@@ -0,0 +1,12 @@
+package org.team2.ecobreakdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoBreakdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoBreakdownApplication.class, args);
+	}
+}
---
+++ src/test/java/org/team2/ecobreakdown/EcoBreakdownApplicationTests.java
@@ -0,0 +1,16 @@
+package org.team2.ecobreakdown;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcoBreakdownApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
